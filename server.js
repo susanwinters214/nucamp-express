a1d49911762b743c4caae2e9f4e0c7915369f28c
Susan Winters <susanwinters214@gmail.com>
2020-08-07 15:06:51 -0500

    StaticFileSetForIndexHTML

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const hostname = 'localhost';
 const port = 3000;
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>Your New Express Server</h1></body></html>');
