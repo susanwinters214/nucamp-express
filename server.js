3fe8e9bc0b179af66b4b9061b1a5b9334a4a89a6
Susan Winters <susanwinters214@gmail.com>
2020-08-07 21:14:56 -0500

    FRIDAYNITE

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
 const hostname = 'localhost';
 const port = 3000;
-const morgan = require('morgan');
-
+const app = express();
 app.use(morgan('dev'));
-
+app.use(bodyParser.json());
+app.use('/campsites', campsiteRouter);
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Your New Express Server</h1></body></html>');
-    });
+app.get((req, res) => {    
+    res.sendFile(__dirname + './index.html') = 200;   
+});
+
 
 app.listen(port, hostname, () => { console.log(`Server running on port ${port}`)});
